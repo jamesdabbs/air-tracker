25b014c8b708718f9459203a892436b91837b67e
stephenkaraga <stephenkaraga@gmail.com>
2014-06-21 14:30:15 -0400

    added model validations for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
   belongs_to :user, foreign_key: "created_by"
-
+  validates :title, presence: true
+  validates :description, presence: true
   has_many :votes
 end
