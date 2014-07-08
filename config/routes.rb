3507632dd04444697e9394420bbfa38a3884bb92
James Dabbs <jamesdabbs@gmail.com>
2014-07-07 22:38:41 -0400

    Clean up the app layout and nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-  
+
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   resources :topics, only: [:index, :new, :create, :show] do
     resource :vote, only: [:create, :destroy]
   end
 
-  root to: "topics#new"
+  root to: "topics#index"
 end
